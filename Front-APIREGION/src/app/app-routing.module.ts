fdcd1c53466c583fc2b316dbaa497bba3229b4df
Aly-D-konte <alykonte19@gmail.com>
2022-12-14 00:53:37 +0100

    interface statique

--- Front-APIREGION/src/app/app-routing.module.ts
+++ Front-APIREGION/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './component/contact/contact.component';
+import { DetailRegionComponent } from './component/detail-region/detail-region.component';
+import { HomeComponent } from './component/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+
+  {
+    path: 'detail',
+    component: DetailRegionComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
