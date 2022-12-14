ec84b98b88a061fad5cd8b6f85fdc0356bac5107
adkonte <alykonte19@gmail.com>
2022-12-14 17:28:36 +0000

    connexion et inscription

--- Front-APIREGION/src/app/app.module.ts
+++ Front-APIREGION/src/app/app.module.ts
@@ -7,8 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './component/home/home.component';
 import { DetailRegionComponent } from './component/detail-region/detail-region.component';
 import { ContactComponent } from './component/contact/contact.component';
+import { RegionComponent } from './component/region/region.component';
+import { InscriptionComponent } from './Compte/inscription/inscription.component';
+import { ConnexionComponent } from './Compte/connexion/connexion.component';
 @NgModule({
-  declarations: [AppComponent, HomeComponent, DetailRegionComponent, ContactComponent],
+  declarations: [AppComponent, HomeComponent, DetailRegionComponent, ContactComponent, RegionComponent, ConnexionComponent, InscriptionComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
